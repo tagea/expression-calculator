a3b1470126c41a85a4d92601c203faa282850026
Yulya Vasilyeva <043@tut.by>
2019-10-12 00:09:39 +0300

    feat: create exression-calculator

--- src/index.js
+++ src/index.js
@@ -11,26 +11,39 @@ function eval(a, b, s) {
     if(s === '*'){
         res = a*b;
     }
-    if(s === '/'){
-        try{
-            res = a/b;
-        }catch(e){
-            throw new Error('TypeError: Division by zero.'); 
-        }
-    }
-    if(res.toFixed().length >2){
-        res = res.toFixed(4);
+    if(s === '/'){   
+       if(b === 0){
+        throw new Error('TypeError: Division by zero.'); 
+       }
+       res = a/b;
     }
     return res;
+}
+
+function evalStr(str){
+    const regSign = /[^\*\!\/\+]+/;
+    for (sign of signArray){
+        while (str.indexOf(sign) !== -1 && str.indexOf(sign) !== 0){
+            const posMulti = str.indexOf(sign);
+            let posMultiLeft = posMulti-1;
+            let posMultiRight = posMulti+1;
+            while(posMultiLeft > 0 && str.charAt(posMultiLeft-1).match(regSign)){
+                posMultiLeft--; 
+            }
+            while(posMultiRight < str.length && str.charAt(posMultiRight).match(regSign)){
+                posMultiRight++; 
+            }
+            
+            str = cutStr(sign,str, posMultiLeft, posMultiRight);
+        }
+    }
+    return str;
 }
 
 
 function deleteSpaces(str){
     const regexp = /\ /gm;
     return str.replace(regexp, '');
-}
-function convertToNumber(arr){
-    return arr.map(item => +item)
 }
 
 function cutStr(sign,mainStr, leftPos, rightPos){
@@ -40,41 +53,86 @@ function cutStr(sign,mainStr, leftPos, rightPos){
     const numberStrRight = cutStr.substring(posMulti+1, cutStr.length);
     const res = eval(+numberStrLeft, +numberStrRight, sign);
     
-    
     return mainStr.replace(cutStr,res);
 }
 
-
-function expressionCalculator(expr) {
-    let slimStr = deleteSpaces(expr);
-    while(slimStr.indexOf('-')!==-1 ) slimStr = slimStr.replace('-','!');
-
-    const regSign = /[^\*\!\/\+]+/;
-    for (sign of signArray){
-        while (slimStr.indexOf(sign) !== -1 && slimStr.indexOf(sign) !== 0){
-            const posMulti = slimStr.indexOf(sign);
-            let posMultiLeft = posMulti-1;
-            let posMultiRight = posMulti+1;
-            while(posMultiLeft > 0 && slimStr.charAt(posMultiLeft-1).match(regSign)){
-                posMultiLeft--; 
-            }
-            while(posMultiRight < slimStr.length && slimStr.charAt(posMultiRight).match(regSign)){
-                posMultiRight++; 
-            }
-            
-            slimStr = cutStr(sign,slimStr, posMultiLeft, posMultiRight);
-            
-            
-            
-            console.log(slimStr);
-            //posMulti = -1;
+function checkBrackets(slimStr, configBrackets){
+    const countBr = [0,0];
+    let tempStr = slimStr;
+    configBrackets.forEach((item, indx) => {
+        while(tempStr.indexOf(item) !== -1){
+            countBr[indx]++;
+            const pos = tempStr.indexOf(item);
+            tempStr = tempStr.substring(pos+1, tempStr.length);
         }
-    }
-    
-    
+        tempStr = slimStr;
+    });
+    return countBr[0] === countBr[1];
 }
 
-expressionCalculator('1/0');
-// module.exports = {
-//     expressionCalculator
-// }
+function findRightBrPos(leftBrPos, str, configBrackets){
+    let found = false;
+    let pos = leftBrPos + 1;
+    let tempFound = 0;
+    while(!found){
+        if(str.charAt(pos) === configBrackets[0]){
+            tempFound++;
+            pos++;
+        }else if(str.charAt(pos) === configBrackets[1]){
+            if(tempFound === 0){found = true;}
+            else{
+                tempFound--;
+                pos++;
+            }
+        }else{
+            pos++;
+        }
+    }
+    return pos;
+}
+
+function expressionCalculator(expr) {
+    const regSign = /[^\*\!\/\+]+/;
+    const configBrackets = ['(',')'];
+    let slimStr = deleteSpaces(expr);
+    // for correct calculate negative values
+    while(slimStr.indexOf('-') !== -1 ) slimStr = slimStr.replace('-','!');
+
+    const bracketsCorrect = checkBrackets(slimStr, configBrackets);
+    if(bracketsCorrect){
+        let fromIndex = 0;
+        let fromLastIndex = slimStr.length;
+        let r = 1
+        while(slimStr.indexOf(configBrackets[0]) !== -1){
+            let leftBrPos = slimStr.indexOf(configBrackets[0],fromIndex);
+            let rightBrPos = findRightBrPos(leftBrPos, slimStr, configBrackets);
+            let slimStrInner = slimStr.substring(leftBrPos+1, rightBrPos);
+            if(slimStrInner.indexOf(configBrackets[0]) === -1){
+                slimStrInner = evalStr(slimStrInner);
+                slimStr = slimStr.substring(0,leftBrPos) + slimStrInner + slimStr.substring(rightBrPos+1,slimStr.length);
+                
+                fromIndex = 0;
+                fromLastIndex = slimStr.length;
+            }else{
+                
+                fromIndex = leftBrPos + 1;
+                fromLastIndex = findRightBrPos(fromIndex, slimStr, configBrackets);
+            }
+        }
+
+        slimStr = evalStr(slimStr);
+        
+        let res = +slimStr;
+        if(slimStr.length > 3){
+            res = res.toFixed(4);
+        }
+        return +res;
+    }else{
+        throw new Error('ExpressionError: Brackets must be paired'); 
+        
+    }
+}
+
+module.exports = {
+    expressionCalculator
+}
